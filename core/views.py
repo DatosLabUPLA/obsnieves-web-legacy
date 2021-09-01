6f0e565eadae63cf10efc98e51294d82521770dc
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2021-09-01 15:17:54 -0400

    Delete: Se remueve páginas no utilizadas

--- core/views.py
+++ core/views.py
@@ -7,26 +7,8 @@ def home(request):
 def team(request):
     return render(request, 'core/team.html')
 
-def team_director(request):
-    return render(request, 'core/team_director.html')
-
-def team_investigator(request):
-    return render(request, 'core/team_investigator.html')
-
-def team_professional(request):
-    return render(request, 'core/team_professional.html')
-
-def team_students(request):
-    return render(request, 'core/team_students.html')
-
 def team_lab(request):
     return render(request, 'core/team_lab.html')
-
-def team_associate(request):
-    return render(request, 'core/team_associate.html')
-
-def team_collab(request):
-    return render(request, 'core/team_collab.html')
 
 
 def database(request):
